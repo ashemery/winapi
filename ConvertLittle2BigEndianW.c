43fde2671feddab5c9167a5ee79d8ec6396558ee
Ali Hadi <ali@ashemery.com>
2022-07-19 03:20:44 -0400

    Delete ConvertLittle2BigEndianW.c

--- ConvertLittle2BigEndianW.c
+++
@@ -1,4 +0,0 @@
-WORD ConvertLittle2BigEndianW(void* lpArray, DWORD dwLocation) {
-        WORD wResult = ((BYTE*)lpArray)[dwLocation + 1];
-    return (wResult << 8) | ((BYTE*)lpArray)[dwLocation];
-};
